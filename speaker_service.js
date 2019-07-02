39a2310be239995ab74245e9bc62c42cf3849ae1
jls232523 <33877502+jls232523@users.noreply.github.com>
2019-07-01 21:58:00 -0700

    Update speaker_service.js

--- speaker_service.js
+++ speaker_service.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
-
 app.use(express.static('public'));
+app.use(express.static(__dirname));
 console.log('web service started');
 
 app.get('/', function (req, res) {
